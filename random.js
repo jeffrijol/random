4f829dc309cd3b94974d2ac8a4491d89859f4d80
Jeffry Castro Bermudez <jeffcb88@gmail.com>
2015-07-24 16:34:50 +0200

    random version 1

---
+++ random.js
@@ -0,0 +1,9 @@
+// Mat.random() devuelve numero aleatorio entre 0 y 1
+var numero = Mat.random();
+
+if(numero<= 0.5);{
+  console.log('\n' + numero + 'menor que 0.5');
+}
+else {
+  console.log('\n' + numero + 'mayor que 0.5');
+}
